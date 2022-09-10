82ee0207d8ad92801f6f53c91d0b01be9533f2d2
VitaliyGO <shuricks007@gmail.com>
2022-09-11 00:16:25 +0300

    create Report v1.0.1

--- JS/script.js
+++ JS/script.js
@@ -16,54 +16,66 @@ const refs = {
     publishers: document.querySelector('#publishers'),
     addBtn: document.querySelector('.js-add-btn'),
     outputBtn: document.querySelector('.js-output-btn'),
+    clearBtn: document.querySelector('.js-clear-btn'),
 }
 
 const REPORT_KEY = "report";
 
 const arrOfInput = Array.from(refs.input);
 
-const report = {
+const report = JSON.parse(localStorage.getItem(REPORT_KEY)) ?? {
     publ: 0,
     video: 0,
     hours: 0,
     visits: 0,
     studies: 0,
+    pubNumbers: 0,
 };
-
-let pubNumbers = 0;
-
-
 
 refs.addBtn.addEventListener('click', onAddBtnClick);
 
 function onAddBtnClick() {
+    if(refs.publ.value.length === 0 || refs.video.value.length === 0 || refs.hours.value.length === 0 || refs.visits.value.length === 0 || refs.studies.value.length === 0){
+        return alert('Всі поля повинні бути заповнені');
+    };
     
     report.publ += Number(refs.publ.value);
     report.video += Number(refs.video.value);
     report.hours += Number(refs.hours.value);
     report.visits += Number(refs.visits.value);
     report.studies += Number(refs.studies.value);
+    report. pubNumbers += 1;
 
+console.log(refs.publ.value.length);
 
     localStorage.setItem(REPORT_KEY, JSON.stringify(report));
 
     arrOfInput.forEach(elem => elem.value = '');
-    pubNumbers += 1;
-    console.log(report);
 };
 
 
-
-refs.outputBtn.addEventListener('click', fillsResultsTable)
-
+refs.outputBtn.addEventListener('click', fillsResultsTable);
 
 function fillsResultsTable() {
     const storageReport = JSON.parse(localStorage.getItem(REPORT_KEY));
+
+    if(!storageReport){
+        refs.resultPubl.textContent = 0;
+        refs.resultVideo.textContent = 0;
+        refs.resultHours.textContent = 0;
+        refs.resultVisits.textContent = 0;
+        refs.resultStudies.textContent = 0;
+        refs.publishers.textContent = 0;
+        return
+       };
 
     refs.resultPubl.textContent = storageReport.publ;
     refs.resultVideo.textContent = storageReport.video;
     refs.resultHours.textContent = storageReport.hours;
     refs.resultVisits.textContent = storageReport.visits;
     refs.resultStudies.textContent = storageReport.studies;
-    refs.publishers.textContent = pubNumbers;
+    refs.publishers.textContent = storageReport.pubNumbers;
 };
+
+refs.clearBtn.addEventListener('click', () => localStorage.removeItem(REPORT_KEY));
+
